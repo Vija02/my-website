b4b80b2c2d1190f7485161b9bca0e5728491aebe
Michael Salim <salim_michaelmi@yahoo.com>
2020-06-19 01:17:21 +0100

    feat: use nprogress, add toast container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,42 @@
 import { AppProps } from 'next/app'
+import Router from "next/router"
+import NProgress from "nprogress"
+import { ToastContainer, toast } from 'react-toastify'
+
 import 'normalize.css'
+import "nprogress/nprogress.css"
 import 'containers/index.css'
+import 'react-toastify/dist/ReactToastify.css'
+
+NProgress.configure({
+  showSpinner: false,
+})
+
+if (typeof window !== "undefined") {
+  Router.events.on("routeChangeStart", () => {
+    NProgress.start()
+  })
+
+  Router.events.on("routeChangeComplete", () => {
+    NProgress.done()
+  })
+  Router.events.on("routeChangeError", (err: Error | string) => {
+    NProgress.done()
+    // @ts-ignore
+    if (err["cancelled"]) {
+      // No need to do anything
+    } else {
+      toast.error("Sorry, the page failed to load. Please try again.")
+    }
+  })
+}
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  )
 }
