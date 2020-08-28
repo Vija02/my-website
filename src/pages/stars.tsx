62df5ff4b18bc01ce17e085ba785b03f1f884e19
Michael Salim <salim_michaelmi@yahoo.com>
2020-08-28 23:20:43 +0100

    feat: add redirect to beyond stars playstore

---
+++ src/pages/stars.tsx
@@ -0,0 +1,10 @@
+function StarsIndex() {
+  return (
+    <>
+      <meta http-equiv="refresh" content="0; url=https://play.google.com/store/apps/details?id=com.beyond.stars" />
+      <p>Redirecting to <a href="https://play.google.com/store/apps/details?id=com.beyond.stars">https://play.google.com/store/apps/details?id=com.beyond.stars</a> ...</p>
+    </>
+  )
+}
+
+export default StarsIndex
