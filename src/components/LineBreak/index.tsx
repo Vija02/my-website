af65dd586642c1d4392835551f07f1ded1ff4fa3
Michael Salim <salim_michaelmi@yahoo.com>
2020-09-04 00:02:50 +0100

    feat: add line break component

---
+++ src/components/LineBreak/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './index.module.css'
+
+export default () => {
+  return (
+    <div className={styles.lineBreak}>
+      <div />
+    </div>
+  )
+}
