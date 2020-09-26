a33e34dc5fac209edc10bad3299302f67cfba701
Michael Salim <salim_michaelmi@yahoo.com>
2020-09-26 11:49:10 +0100

    fix: wrong tag

--- src/pages/stars.tsx
+++ src/pages/stars.tsx
@@ -1,7 +1,7 @@
 function StarsIndex() {
   return (
     <>
-      <meta http-equiv="refresh" content="0; url=https://play.google.com/store/apps/details?id=com.beyond.stars" />
+      <meta httpEquiv="refresh" content="0; url=https://play.google.com/store/apps/details?id=com.beyond.stars" />
       <p>Redirecting to <a href="https://play.google.com/store/apps/details?id=com.beyond.stars">https://play.google.com/store/apps/details?id=com.beyond.stars</a> ...</p>
     </>
   )
