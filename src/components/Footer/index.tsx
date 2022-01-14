ea2a0180b1e31e1c7c6b8a5d8c9392fd20958c9b
Michael Salim <salim_michaelmi@yahoo.com>
2022-01-14 18:05:33 +0000

    Feat: Add link to twitter

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,12 @@
 import React from "react"
-import Link from 'next/link'
+import Link from "next/link"
 
-import styles from './index.module.css'
+import styles from "./index.module.css"
 
-import Favicon from '../../../public/assets/svg/icon.svg'
+import Favicon from "../../../public/assets/svg/icon.svg"
 import Linkedin from "assets/logo/linkedin-logo.svg"
 import Github from "assets/logo/github-logo.svg"
+import Twitter from "assets/logo/twitter-logo.svg"
 
 export default () => {
   return (
@@ -13,24 +14,45 @@ export default () => {
       <div className={styles.heightPad} />
       <div className={styles.absoluteContainer}>
         <div className={styles.container}>
-          <Link href="/"><a>Home</a></Link>
-          <Link href="/about-me"><a>About</a></Link>
-          <Link href="/dev-notes"><a>Dev Notes</a></Link>
-          <Link href="/contact-me"><a>Contact</a></Link>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/about-me">
+            <a>About</a>
+          </Link>
+          <Link href="/dev-notes">
+            <a>Dev Notes</a>
+          </Link>
+          <Link href="/contact-me">
+            <a>Contact</a>
+          </Link>
           <div className={styles.socialMedia}>
-            <a href="https://www.linkedin.com/in/michael-salim/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://twitter.com/IamMichaelSalim/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Twitter />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/michael-salim/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <Linkedin />
             </a>
-            <a href="https://github.com/vija02/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/vija02/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <Github />
             </a>
           </div>
           <div className={styles.bottom}>
             <Favicon />
             <span className={styles.verse}>— John 3:16 —</span>
-            <span>
-              © {new Date().getFullYear()} Michael Salim
-          </span>
+            <span>© {new Date().getFullYear()} Michael Salim</span>
           </div>
         </div>
       </div>
--- src/containers/Home/Hero/index.tsx
+++ src/containers/Home/Hero/index.tsx
@@ -5,8 +5,9 @@ import dynamic from "next/dynamic"
 import ChevronDown from "assets/chevron-down.svg"
 import Linkedin from "assets/logo/linkedin-logo.svg"
 import Github from "assets/logo/github-logo.svg"
+import Twitter from "assets/logo/twitter-logo.svg"
 
-import styles from './index.module.css'
+import styles from "./index.module.css"
 
 const Canvas = dynamic(() => import("./Canvas"), {
   ssr: false,
@@ -23,19 +24,44 @@ export default () => {
           <h1>MICHAEL</h1>
           <h1>SALIM</h1>
           <h2>Full Stack Freelancer</h2>
-          <h3>I make <u>your product</u> a reality.</h3>
-          <h3>Simply <i>imagine</i> and consider it done.</h3>
+          <h3>
+            I make <u>your product</u> a reality.
+          </h3>
+          <h3>
+            Simply <i>imagine</i> and consider it done.
+          </h3>
         </div>
         <div className={styles.rightContainer}>
-          <Link href="/about-me"><a>About</a></Link>
+          <Link href="/about-me">
+            <a>About</a>
+          </Link>
           {/* <a>Blog</a> */}
-          <Link href="/dev-notes"><a>Dev Notes</a></Link>
-          <Link href="/contact-me"><a>Contact</a></Link>
+          <Link href="/dev-notes">
+            <a>Dev Notes</a>
+          </Link>
+          <Link href="/contact-me">
+            <a>Contact</a>
+          </Link>
           <div className={styles.socialMedia}>
-            <a href="https://www.linkedin.com/in/michael-salim/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://twitter.com/IamMichaelSalim/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Twitter />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/michael-salim/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <Linkedin />
             </a>
-            <a href="https://github.com/vija02/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/vija02/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <Github />
             </a>
           </div>
