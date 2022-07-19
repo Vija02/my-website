c7c5a4ebbd68aa7c877590e7422ce68f404e5dc9
Michael Salim <salim_michaelmi@yahoo.com>
2022-07-19 09:45:39 +0100

    Feat: Home recall banner

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -2,11 +2,13 @@ import HomeHero from './Hero'
 import HomePitch from './Pitch'
 import HomeProcess from './Process'
 import HomeContact from './Contact'
+import HomeRecall from './Recall'
 
 export default () => {
   return (
     <>
       <HomeHero />
+      <HomeRecall />
       <HomePitch />
       <HomeProcess />
       <HomeContact />
